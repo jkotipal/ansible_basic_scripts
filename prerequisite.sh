1b509d7ed3423136d9bc69c975c3011ab2c00f91
Ubuntu <ubuntu@ip-172-31-18-241.eu-north-1.compute.internal>
2025-04-23 04:26:15 +0000

    this is my first commit

---
+++ prerequisite.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Clear the terminal for readability
+clear
+set -e
+
+# Update and upgrade packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install Python3, pip, and venv
+sudo apt-get install -y python3 python3-pip python3-venv
+
+# Create and activate a virtual environment
+python3 -m venv ~/.env
+source ~/.env/bin/activate
+
+# Install boto3 inside the virtual environment
+pip install boto3
+
+# Install dependencies and Ansible
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+
+# Install Git
+sudo apt-get install -y git
+
+# Display versions to verify installation
+echo
+echo "Installed Versions:"
+python3 --version
+pip3 --version
+ansible --version | head -n 1
+git --version
+~/.env/bin/python -m pip show boto3 | grep Version
+
